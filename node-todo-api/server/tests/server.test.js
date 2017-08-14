10739af1294aa1d10942d7deef627d6dd6673eb7
lewis-david <lewis_david@bah.com>
2017-08-14 11:14:46 -0400

    add tests for GET /todos

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -4,17 +4,23 @@ const expect = require('expect');
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo');
 
-//run code before test case; in this case, make sure database is empty
-//automatically runs before each test case ('it')
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+//automatically run code before test case; in this case, make sure database is empty
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 //test if a post worked correctly
 describe('POST /todos', () => {
   //new todo created properly
   it('should create a new todo', (done) => {
-    var text = 'testing todo text';
+    var text = 'Test todo text';
 
     request(app)
       .post('/todos')
@@ -29,7 +35,7 @@ describe('POST /todos', () => {
         }
 
         //verify todo was added (fetch data)
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -49,9 +55,22 @@ describe('POST /todos', () => {
         }
       //verify length of todos is 0
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch( (err) => done(err) );
     });
   });
 });
+
+//test if GET worked correctly
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
+})
